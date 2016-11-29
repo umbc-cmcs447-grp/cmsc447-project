34fda675491682fc7c2fee63ab29bb5bc6c44a5a
Donnell Muse <qq45691@umbc.edu>
2016-11-28 21:35:13 -0500

    updated createPost

---
+++ createPost.js
@@ -0,0 +1,38 @@
+function createPost(title, body, category){
+    var newPost = new NetBuz.NewPost(title, body, category);
+
+    console.log("here");
+
+    function success(){
+        alert("Post Successful");
+    }
+
+    function failure(){
+    	alert("failed");
+    }
+    NetBuz.createPost(newPost,success,failure);
+}
+
+function validation(){
+
+    var title = document.getElementByID("title").value;
+    var body = document.getElementByID("body").value;
+    var category = document.getElementByID("category").value;
+
+    if(empty(title)){
+        alert("Invalid Title. Try Again");
+        return false;
+    }
+    if(empty(body)){
+        alert("Invalid Body. Try Again");
+        return false;
+    }
+    if(empty(category)){
+        alert("Invalid Category. Try Again");
+    }
+    return true;
+}
+
+function empty(str){
+    return (!str || 0 === str.length || /^\s*$/.test(str))
+}
