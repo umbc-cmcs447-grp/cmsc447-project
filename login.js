ea8719d1e5b8a277e4e24190bf538be79c31f67e
ChenKuo <ckuo1@umbc.edu>
2016-11-29 03:05:22 -0500

    log in

--- login.js
+++ login.js
@@ -1,13 +1,46 @@
-function validate()
-{
-	var password1 = document.getElementById("password").value;
-	var username = document.getElementById("username").value;
-	
-	if(!username || !password1)
+"use strict";
+const updateLoggedInView=(loggedInID)=>{
+	if(loggedInID){
+		$("#logged_in_message").html("You are logged in as <b>"+loggedInID+"</b>.");
+		$(".show-when-logged-in").show();
+		$(".show-when-logged-out").hide();
+	}
+	else{
+		$(".show-when-logged-in").hide();
+		$(".show-when-logged-out").show();
+	}
+}
+
+const handleLogin=()=>{
+	var password = document.getElementById("password").value;
+	var userID = document.getElementById("user_id").value;
+	var fieldset=document.getElementById("form_fieldset");
+
+	if(!userID || !password)
 	{
 		alert("No input enter");
 		return false;
 	}
-	return true;
-	
+	fieldset.disabled=true;
+	NetBuz.login(userID, password,
+		()=>{
+			var loggedInID=NetBuz.getLoggedInId();
+			updateLoggedInView(loggedInID);
+		},
+		(xhr)=>{
+			if(xhr.status===401){
+				alert("Incorrect ID or Password.");
+			}else{
+				alert("An error occurred. You are not logged in.");
+				fieldset.disabled=false;
+			}
+		}
+	);
+	return false;
 }
+
+//entry point
+$(()=>{
+	var loggedInID=NetBuz.getLoggedInId();
+	updateLoggedInView(loggedInID);
+});
